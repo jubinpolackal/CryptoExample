9ab4f6e7dbc6fd3f1a9b93deae3d57c7f0ed7f1a
jjose <jubin.jose@kanayo.com>
2021-09-22 14:41:53 -0300

    first commit

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core'
 
 import { Item } from './item'
 import { ItemService } from './item.service'
+import {bcrypt} from 'nativescript-bcryptjs';
+var CryptoJS = require("crypto-js");
 
 @Component({
   selector: 'ns-items',
@@ -9,10 +11,17 @@ import { ItemService } from './item.service'
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
-
+  seed = "UjXn2rm5u8x/A?D(G+KmaPdSgVkYp3sm6v9";
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
     this.items = this.itemService.getItems()
+    let key = this.seed.split( "m").join("")
+    var bytes  = CryptoJS.AES.decrypt("ishgjshgjfdghjkshlkghjdkgjdkgjfdgb", key);
+  }
+
+  hashPassword( password: string): string {
+    let salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
   }
 }
